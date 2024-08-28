e187b3c89e0beec72c0f924b1356d6ddd66e4a16
Syed Mehdi <mehdisyed3@gmail.com>
2024-08-28 14:40:03 -0400

    utils

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+// to use any middleware we use app.use
+app.use(cors({
+  origin : process.env.CORS_ORIGIN, // to only allow cross origin resouce sharing from the specified origin
+  credentials : true
+}));
+
+app.use(express.json({limit: '16kb'})); // to receive json data from the client with a limit of 16kb
+app.use(express.urlencoded({extended: true, limit: '16kb'})); //to receive and parse urlencoded data from the client with a limit of 16kb 
+app.use(express.static('public'));// to serve static files from the public folder
+app.use(cookieParser()); // to parse cookies from the client and perdorm crud operations on them
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path:'./env'
 });
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+.then(
+  () => { 
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
+)
+.catch((error) => {
+  console.log('MONGODB connection Faill err :',error);
+  process.exit(1);
+});
