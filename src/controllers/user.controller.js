c7581a223b7639caa124b9d9e369536e1031e2ca
Syed Mehdi <mehdisyed3@gmail.com>
2024-09-09 15:36:15 -0400

    added register user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,66 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "user registered!!!!" });
+  // res.status(200).json({ message: "user registered!!!!" });
+  const { fullName, email, password, username } = req.body;
+
+  if([fullName, email, password, username].some((field) => field?.trim() === '')){
+    throw new ApiError(400, 'All fields are required');
+  }
+
+ const existingUser = User.findOne({ 
+    $or: [ { email }, { username } ]
+   })
+
+   if(existingUser){
+     throw new ApiError(409, 'Email or username already exists');
+   }
+
+  const avatarLocalPath =  req.files?.avatar[0]?.path;
+  const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath){
+    throw new ApiError(400, 'Avatar is required');
+  }
+  // if(!coverImageLocalPath){
+  //   throw new ApiError(400, 'Cover Image is required');
+  // }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!avatar){
+    throw new ApiError(500, 'Error uploading avatar');
+  }
+  // if(!coverImage){
+  //   throw new ApiError(500, 'Error uploading cover image');
+  // }
+  const  user = await User.create({
+    fullName,
+    username: username.toLowerCase(),
+    password,
+    email,
+    avatar: avatar.url,
+    coverImage: coverImage?.url||'',    
+  })
+
+  const createdUser = await User.findById(user._id).select('-password -refreshToken');
+
+  if(!createdUser){
+    throw new ApiError(500, 'Error creating user');
+  }
+
+  return res.status(201).json(
+    new ApiResponse(201, 'User created successfully', createdUser)
+  );
 });
 
 const logginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "user logged in Cheetayy !!!!" });
+  res.status(20).json({ message: "user logged in Cheetayy !!!!" });
 });
 
 export { registerUser,logginUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { logginUser, registerUser } from "../controllers/user.controller.js";
 
+import { upload } from "../middlewares/multer.middleware.js";
+
 const router = Router();
 
-router.route('/register').post(registerUser)
+router.route('/register').post(
+  upload.fields([
+    {
+      name: "avatar", maxCount: 1
+    },
+    {
+      name: "coverImage", maxCount: 1
+    }
+  ]),
+  registerUser
+)
 router.route('/login').get(logginUser)
 
 export default router;
