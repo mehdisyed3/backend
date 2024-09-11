2bfd3a98e6701c9722a849e96e5e5fb1afe0c032
Syed Mehdi <mehdisyed3@gmail.com>
2024-09-11 16:14:43 -0400

    commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,17 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// multer middleware is used to upload files to the server
+// upload.fields() is used to upload multiple files to the server
+// upload.fields() takes an array of objects with the name and maxCount properties
+// name is the name of the field in the form
+// maxCount is the maximum number of files that can be uploaded
+// upload.fields() returns a middleware function that can be used in the route
+// upload.fields() saves the files in the specified destination
+// upload.fields() saves the file information in the req.files object
+// upload.fields() saves the file path in the req.files[field][index].path property
+// upload.fields() saves the file original name in the req.files[field][index].originalname property
+// upload.fields() saves the file mimetype in the req.files[field][index].mimetype property
 router.route('/register').post(
   upload.fields([
     {
@@ -17,6 +28,7 @@ router.route('/register').post(
   ]),
   registerUser
 )
+
 router.route('/login').post(logginUser)
 
 //secure routes
